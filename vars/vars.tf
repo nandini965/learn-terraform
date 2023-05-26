dd782c49db627e839c53c8e38d022da330e50841
nandini965 <nandhini965@gmail.com>
2023-05-26 15:08:01 +0530

    server.tf

--- vars/vars.tf
+++ vars/vars.tf
@@ -1,13 +1,3 @@
 variable "sample_string" {
   default = "Hello world"
 }
-##shell scripting equilent sample_string = "hello world"
-##To print in shell script we use echo , following is the syntax in shell
-## echo $ sample_string
- output "sample_string" {
-   value = var.sample_string
- }
-## in above case we are printing just variable, if variable is a combination of some strings then following is the synthax. {} is mandatory if we include the variable inside a string
-output "sample_string1" {
-  value = "value of sample_string = $ {var.sample_string}"
-}
