12a568c0f5dcb9691040abe9d6852ca29059c91f
nandini965 <nandhini965@gmail.com>
2023-05-26 15:52:33 +0530

    servers.tf

--- vars/output.tf
+++ vars/output.tf
@@ -8,3 +8,17 @@ output "sample_string" {
 output "sample_string1" {
   value = "value of sample_string = $ {var.sample_string}"
 }
+## values from list
+##second value in list
+output "sample_list_2" {
+  value = var.sample_list[1]
+
+}
+
+print number1 in samplesample_dict
+output "sample_dict_num1" {
+  value = var.sample_dict["number1"]
+}
+output "sample_dict_bool" {
+  value = var.sample_dict["boolean"]
+}
--- vars/vars.tf
+++ vars/vars.tf
@@ -1,3 +1,34 @@
 variable "sample_string" {
   default = "Hello world"
 }
+variable = "sample_number" {
+ default = 100
+}
+# 10.6 is also a number in terraform
+variable "sample_boolean" {
+  default = true
+}
+## booleans and numbers doesnot need to be quoted, only strings need to be  quoted that to be with double quotes. terraform does support for single quotes
+### variable types
+# so far we have seen is default variable type
+## we have list variable type , meanig single variable can hold mutiple values
+variable "sample_list" {
+        default = [
+        100;
+        "hello",
+        true,
+        123
+]
+}
+##data type inside the list doesnot matter in terrform
+
+#we have a dictionary (or maps) < meaning single variable will have multiple values, & each value s going to have a separate name
+variable "sample_dict" {
+deafalt = {
+number1 = 100
+string1 + "hello"
+number2 = 123
+boolean = true
+}
+}
+## this is the most preferred variable over the list when we declare the data
