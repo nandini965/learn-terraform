5a63ae6fe1891a169e862e1a4139919a5efd7d29
nandini965 <nandhini965@gmail.com>
2023-05-31 19:20:32 +0530

    servers.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ output "security_group_id" {
 value = data.aws_security_group.selected.id
 }
 
-data "aws_security_groups" "test" {
+data "aws_security_groups" "test" {}
   output "all_sg" {
     value = data.aws_security_groups_test
   }
