7903546df801b2107e0d18b9d870c0e3aab8e168
nandini965 <nandhini965@gmail.com>
2023-06-01 09:00:00 +0530

    servers.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,9 @@
 
 data "aws_security_group" "test" {}
 data "aws_security_group" "single" {
-  count = length(data.aws_security_groups.test.ids)
-  id = data.aws_security_groups.test.ids [count.index]
+  count = "length(data.aws_security_groups.test.ids)"
+  id = "data.aws_security_groups.test.ids [count.index]"
 }
 output "all_sg" {
-  value = data.aws_security_group_test
+  value = "data.aws_security_group_test"
 }
