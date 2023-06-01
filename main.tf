96cc60fd461f712f8350902e847340083c6a2f6b
nandini965 <nandhini965@gmail.com>
2023-06-01 08:54:05 +0530

    servers.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,12 @@
 //value = data.aws_security_group.selected.id
 //}
 
-data "aws_security_groups" "test" {}
-  output "all_sg" {
-    value = "data.aws_security_groups_test"
-  }
+
+data "aws_security_group" "test" {}
+data "aws_security_group" "single" {
+  count = length(data.aws_security_groups.test.ids)
+  id = data.aws_security_groups.test.ids [count.index]
+}
+output "all_sg" {
+  value = data.aws_security_group_test
+}
