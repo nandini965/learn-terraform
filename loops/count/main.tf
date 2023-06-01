2b445cc2b1672279c7878eabfe6d48faafec7598
nandini965 <nandhini965@gmail.com>
2023-06-01 09:51:39 +0530

    servers.tf

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -5,6 +5,9 @@
 resource "null_resource" "fruits" {
   count = length(var.fruits)
 }
+provisioner "local-exec" {
+command = "echo ${var.fruits[count.index]}"
+  }
 variable "fruits" {
   default = ["apple", "banana", "orange"]
 }
