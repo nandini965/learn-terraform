75b8465ff35ef822d5a2f61cc60ee44a24d9c5b1
nandini965 <nandhini965@gmail.com>
2023-06-01 15:21:33 +0530

    servers.tf

--- loops/count/for.each/main.tf
+++ loops/count/for.each/main.tf
@@ -12,3 +12,28 @@ variable "fruits" {
     mango =30
   }
 }
+
+
+resource "null_resource" "fruits1" {
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+      command = "echo Fruit Name - ${each.key} - ${each.value["count"]}"
+
+  }
+}
+variable "fruits1" {
+   default = {
+     apple = {
+       name  = "apple"
+       count = 10
+     }
+     orange = {
+       name  = "orange"
+       count = 10
+     }
+     banana = {
+       name  = "banana"
+       count = 100
+     }
+   }
