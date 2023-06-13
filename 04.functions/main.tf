b2e9460a1a341607731832e3ae06a6117a761ddc
nandini965 <nandhini965@gmail.com>
2023-06-13 18:47:17 +0530

    state

--- 04.functions/main.tf
+++ 04.functions/main.tf
@@ -6,3 +6,11 @@ variable "sample" {
 output "sample" {
   value = upper(var.sample)
 }
+
+
+variable "sample1" {
+  default = ["abc","xyz"]
+}
+output "sample1"{
+  value = element(var.sample1,0)
+}
