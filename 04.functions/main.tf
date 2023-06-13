ac39cd7eacdc333c1050dbcaf46b801153538d7a
nandini965 <nandhini965@gmail.com>
2023-06-13 18:54:20 +0530

    state

--- 04.functions/main.tf
+++ 04.functions/main.tf
@@ -11,6 +11,16 @@ output "sample" {
 variable "sample1" {
   default = ["abc","xyz"]
 }
-output "sample1"{
-  value = element(var.sample1,0)
+output "sample1" {
+  value = element(var.sample1, 0)
+}
+
+  variable "sample2"{
+    default = {
+      xyz = 100
+      abc = 200
+  }
+  }
+output "sample2"{
+  value = lookup(var.sample2,"a1","dummy")
 }
