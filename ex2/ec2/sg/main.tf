10fa99a900af3d72ab84ab251d4649dca0688cf3
nandini965 <nandhini965@gmail.com>
2023-06-17 18:12:51 +0530

    state

--- ex2/ec2/sg/main.tf
+++ ex2/ec2/sg/main.tf
@@ -14,7 +14,7 @@
     to_port       = 0
     protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
-      ipv6_cdir_blocks = ["::/0"]
+      "ipv6_cidr_blocks" = ["::/0"]
     }
   tags = {
   name = "allow_tls"
