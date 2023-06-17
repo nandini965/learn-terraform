9478c0cc00c060be06508df13c0632e1db33a0b9
nandini965 <nandhini965@gmail.com>
2023-06-17 16:21:21 +0530

    state

--- 06.module/module/main.tf
+++ 06.module/module/main.tf
@@ -1,15 +1,15 @@
 resource "null_resource" "nothing" {
   provisioner "local-exec" {
-    command = "echo hello from module-input-${var.input}"
+    command = "echo hello from module -input-${var.input}"
   }
 }
 resource "null_resource" "nothing2" {
   provisioner "local-exec" {
-    command = "echo hello from module-input-${var.input}"
+    command = "echo hello from module -input-${var.input}"
   }
 }
 resource "null_resource" "nothing3" {
   provisioner "local-exec" {
-    command = "echo hello from module-input-${var.input}"
+    command = "echo hello from module -input-${var.input}"
   }
 }
