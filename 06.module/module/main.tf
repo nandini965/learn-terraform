e6eba651f5ff4960ebc4fb80920ecdd9bf43fe9e
nandini965 <nandhini965@gmail.com>
2023-06-17 16:12:40 +0530

    state

--- 06.module/module/main.tf
+++ 06.module/module/main.tf
@@ -1,17 +1,14 @@
-source "null_resource" "nothing" {
+resource "null_resource" "nothing" {
   provisioner "local-exec" {
     command = "echo hello from module"
   }
 }
-
-
-source "null_resource" "nothing2" {
+resource "null_resource" "nothing2" {
   provisioner "local-exec" {
     command = "echo hello from module"
   }
 }
-
-source "null_resource" "nothing3" {
+resource "null_resource" "nothing3" {
   provisioner "local-exec" {
     command = "echo hello from module"
   }
